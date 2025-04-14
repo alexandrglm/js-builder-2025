1f6908c7c10b7fb6173339d914f27c270ffea3af
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-04-14 12:07:04 +0200

    Update prod.config.js

--- templates/es6-starter/webpack/prod.config.js
+++ templates/es6-starter/webpack/prod.config.js
@@ -1,125 +1,113 @@
-const path = require("path");
-const webpackMerge = require("webpack-merge");
-const autoprefixer = require("autoprefixer");
-const webpackCommon = require("./common.config");
+/*
+FIXED TO 2025
+
+Pendning to explain changes
+*/
+
+const path = require('path');
+const { merge } = require('webpack-merge');
+const webpackCommon = require('./common.config');
 
 // webpack plugins
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DefinePlugin = require("webpack/lib/DefinePlugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const LoaderOptionsPlugin = require("webpack/lib/LoaderOptionsPlugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = webpackMerge(webpackCommon, {
+module.exports = merge(webpackCommon, {
   bail: true,
-
-  devtool: "source-map",
-  mode: "production",
+  devtool: 'source-map',
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, "../dist"),
-
-    filename: "[name]-[hash].min.js",
-
-    sourceMapFilename: "[name]-[hash].map",
-
-    chunkFilename: "[id]-[chunkhash].js",
-
-    publicPath: "/"
+    path: path.resolve(__dirname, '../dist'),
+                       filename: '[name]-[contenthash].min.js',
+                       chunkFilename: '[id]-[contenthash].js',
+                       publicPath: '/',
+                         assetModuleFilename: '[name]-[hash][ext]'
   },
 
   module: {
     rules: [
       {
         test: /\.s?css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: true,
-                importLoaders: 2
-              }
-            },
-            {
-              loader: "postcss-loader",
-              options: {
-                config: {
-                  path: path.resolve(__dirname, "postcss.config.js")
-                },
-                sourceMap: true
-              }
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                outputStyle: "expanded",
-                sourceMap: true,
-                sourceMapContents: true
-              }
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: true
             }
-          ]
-        })
+          },
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       }
+    ]
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            warnings: false
+          },
+          output: {
+            comments: false
+          }
+        },
+        extractComments: false
+      }),
+      new CssMinimizerPlugin()
     ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: path.resolve(__dirname, "../static/index.html"),
-      favicon: path.resolve(__dirname, "../static/favicon.ico"),
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true
-      }
+      template: path.resolve(__dirname, '../static/index.html'),
+                          favicon: path.resolve(__dirname, '../static/favicon.ico'),
+                          minify: {
+                            removeComments: true,
+                            collapseWhitespace: true,
+                            removeRedundantAttributes: true,
+                            useShortDoctype: true,
+                            removeEmptyAttributes: true,
+                            removeStyleLinkTypeAttributes: true,
+                            keepClosingSlash: true,
+                            minifyJS: true,
+                            minifyCSS: true,
+                            minifyURLs: true
+                          }
     }),
-    new CopyWebpackPlugin([{ from: path.resolve(__dirname, "../static") }], {
-      ignore: ["index.html", "favicon.ico"]
-    }),
-    new CleanWebpackPlugin(["dist"], {
-      root: path.resolve(__dirname, ".."),
-      exclude: ".gitignore"
-    }),
-    new DefinePlugin({
-      "process.env": {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new ExtractTextPlugin("[name]-[chunkhash].min.css"),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        compress: {
-          ie8: true,
-          warnings: false
-        },
-        mangle: {
-          ie8: true
-        },
-        output: {
-          comments: false,
-          ie8: true
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../static'),
+                   globOptions: {
+                     ignore: ['**/index.html', '**/favicon.ico']
+                   }
         }
-      },
-      sourceMap: true
+      ]
     }),
-    new LoaderOptionsPlugin({
-      options: {
-        context: "/",
-        sassLoader: {
-          includePaths: [path.resolve(__dirname, "../src")]
-        }
-      }
-    })
+    new CleanWebpackPlugin(),
+                       new DefinePlugin({
+                         'process.env': {
+                           NODE_ENV: JSON.stringify('production')
+                         }
+                       }),
+                       new MiniCssExtractPlugin({
+                         filename: '[name]-[contenthash].min.css'
+                       })
   ]
 });
